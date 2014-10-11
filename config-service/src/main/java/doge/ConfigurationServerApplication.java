c6e13bef0dafdba47e45b85aefeb07d719c34c48
Josh Long <josh@joshlong.com or josh.long@springsource.com>
2014-10-10 22:42:23 -0700

    polish

--- config-service/src/main/java/doge/ConfigurationServerApplication.java
+++ config-service/src/main/java/doge/ConfigurationServerApplication.java
@@ -1,11 +1,14 @@
 package doge;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 /**
  * This will look in the .git repository configured in {@code application.yml}
@@ -17,7 +20,8 @@ import org.springframework.context.annotation.Configuration;
  * <p>
  * You can confirm that this works by visiting $URI/$APP_NAME/default. This will show you for example the values in
  * {@code doges.yml} if your {@code $APP_NAME} is 'doges'
- * <P>
+ * <p>
+ *
  * @author Josh Long
  */
 @Configuration
@@ -26,6 +30,16 @@ import org.springframework.context.annotation.Configuration;
 @EnableEurekaClient
 @EnableConfigServer
 public class ConfigurationServerApplication {
+
+    @Bean
+    CommandLineRunner init(Environment e) {
+        return args -> {
+            System.out.println("default profile: " + e.getProperty("spring.profiles.default"));
+            System.out.println("eureka credentials: " + e.getProperty("vcap.services.eureka-service.credentials.uri"));
+            System.out.println("eureka.client.serviceUrl.defaultZone: " + e.getProperty("eureka.client.serviceUrl.defaultZone"));
+            System.out.println("eureka.client.instance.hostname: " + e.getProperty("eureka.client.instance.hostname"));
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigurationServerApplication.class, args);
